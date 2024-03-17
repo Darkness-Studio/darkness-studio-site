db630599e13cccd3a5673e8c1b447051321e4439
Wesley Queiroz <wesleyqueirozdeveloper@gmail.com>
2024-03-17 18:44:38 -0300

    style(prettier): configuring Eslint and adding Prettier
    Install @rocketseat/eslint-config; Update Eslint from version 8 to 8.57.0; Adding
    @rocketseat/eslint-config/next to .eslintrc.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-export default function Home(){
+export default function Home() {
   return (
     <>
       <main className="flex h-screen">
         <div className="m-auto">
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
-          <p>
-            This is the home page.
-          </p>
+          <h1 className="text-3xl font-bold underline">Helloworld! </h1>
+          <p>This is the home page.</p>
         </div>
       </main>
     </>
